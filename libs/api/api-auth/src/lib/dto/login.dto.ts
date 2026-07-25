6199e9aa33add7312a0ecbf26cd91b22f0e6e349
SpiLlett777 <spillettnettbrett@gmail.com>
2026-07-25 04:52:38 +0300

    chore(auth): добавить документацию для logindto и registerdto

--- libs/api/api-auth/src/lib/dto/login.dto.ts
+++ libs/api/api-auth/src/lib/dto/login.dto.ts
@@ -6,8 +6,10 @@ import {
   MinLength,
 } from 'class-validator';
 import { authConfig } from '@wm/shared/auth';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty()
   @IsDefined({ message: authConfig.email.required.message })
   @IsString()
   @Matches(authConfig.email.correct.pattern, {
@@ -18,6 +20,7 @@ export class LoginDto {
   })
   email!: string;
 
+  @ApiProperty()
   @IsDefined({ message: authConfig.password.required.message })
   @IsString()
   @MinLength(authConfig.password.minLength.value, {
