c1bafca7ca7fdf6f4a84a5893561435fb405a25a
SpiLlett777 <spillettnettbrett@gmail.com>
2026-08-02 03:48:17 +0300

    refactor(web): применить новое контекстное меню на аватарке хедера

--- libs/web/layout/src/lib/base/header/header.component.ts
+++ libs/web/layout/src/lib/base/header/header.component.ts
@@ -3,13 +3,22 @@ import {
   Component,
   inject,
   Renderer2,
+  Signal,
   signal,
+  TemplateRef,
+  viewChild,
 } from '@angular/core';
-import { SearchInputComponent, SvgComponent } from '@wm/web/common-ui';
+import {
+  ContextMenuTriggerDirective,
+  SearchInputComponent,
+  SvgComponent,
+} from '@wm/web/common-ui';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '@wm/web/data-access/auth';
 import { firstValueFrom } from 'rxjs';
+import { ContextMenuOption } from '@wm/web/data-access/shared';
+import { ContextMenuItemComponent } from '@wm/web/common-ui';
 
 @Component({
   selector: 'wm-header',
@@ -19,6 +28,8 @@ import { firstValueFrom } from 'rxjs';
     RouterLink,
     ReactiveFormsModule,
     RouterLinkActive,
+    ContextMenuTriggerDirective,
+    ContextMenuItemComponent,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
@@ -26,12 +37,17 @@ import { firstValueFrom } from 'rxjs';
 })
 export class HeaderComponent {
   #authService = inject(AuthService);
+  #router = inject(Router);
+
+  r2 = inject(Renderer2);
 
   searchControl = new FormControl('');
 
   isMenuOpen = signal<boolean>(false);
 
-  r2 = inject(Renderer2);
+  contextMenuTemplate: Signal<TemplateRef<any> | undefined> = viewChild(
+    'contextMenuTemplate',
+  );
 
   toggleMenu() {
     this.isMenuOpen.set(!this.isMenuOpen());
@@ -47,5 +63,29 @@ export class HeaderComponent {
 
   logout() {
     firstValueFrom(this.#authService.logout()).then();
+  }
+
+  menuOptions: ContextMenuOption[] = [
+    {
+      label: 'Вставить',
+      extra: 'Ctrl + V',
+      disabled: true,
+      action: () => console.log('Вставили'),
+    },
+    {
+      label: 'Удалить',
+      icon: 'share',
+      extra: 'Del',
+      action: () => console.log('DELETE'),
+    },
+    {
+      label: 'Профиль',
+      icon: 'avatar',
+      action: () => this.navigateToProfile(),
+    },
+  ];
+
+  private navigateToProfile() {
+    this.#router.navigate(['profile', 'me']).then();
   }
 }
